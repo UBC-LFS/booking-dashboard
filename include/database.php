bc4d9cade78b16d4f48db7072ca49091b73b07ea
edmundseow <edmund.seow@ubc.ca>
2022-08-19 14:18:39 -0700

    DB connect and display

---
+++ include/database.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'conf/mysql.inc';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
