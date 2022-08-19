3b8c183045c196fcc0a7dff3a901ec5e89e33b47
edmundseow <edmund.seow@ubc.ca>
2022-08-19 14:46:19 -0700

    Updated conn spec and query

--- include/database.php
+++ include/database.php
@@ -3,22 +3,26 @@
 require 'conf/mysql.inc';
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+//Query all bookings for today
+$query = "SELECT mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)";
+
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
+  echo "ITEM | Notes | User <br>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo $row["room_name"]. " | " . $row["name"]. " | " . $row["user_email"]. "<br>";
   }
 } else {
-  echo "0 results";
+  echo "<p>There are no bookings today.";
 }
 $conn->close();
 ?>
