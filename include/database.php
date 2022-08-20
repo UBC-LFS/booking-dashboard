c8834912718171667182d8ccc06fd68a3de7b6d8
Edmund Seow <edmund.seow@ubc.ca>
2022-08-20 12:05:37 -0700

    Removed display code

--- include/database.php
+++ include/database.php
@@ -2,27 +2,24 @@
 
 require 'conf/mysql.inc';
 
-// Create connection
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+function get-bookings() {
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-//Query all bookings for today
-$query = "SELECT mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)";
-
-$result = $conn->query($query);
-
-if ($result->num_rows > 0) {
-  echo "ITEM | Notes | User <br>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo $row["room_name"]. " | " . $row["name"]. " | " . $row["user_email"]. "<br>";
+  // Create connection
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+  
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
   }
-} else {
-  echo "<p>There are no bookings today.";
+  
+  //Query all bookings for today
+  $query = "SELECT mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)";
+  
+  $result =  $conn->query($query);
+
+  $conn->close();
+
+  return $result;
+
 }
-$conn->close();
 ?>
