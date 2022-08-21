f26e239d3b67228f72b501051ff8b658c10e133b
Edmund Seow <edmund.seow@ubc.ca>
2022-08-20 21:17:06 -0700

    Updated key

--- include/queries.php
+++ include/queries.php
@@ -2,7 +2,7 @@
 
 $query = array (
   "today" => "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)",
-  "next_5_days" => "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 DAY)"
+  "next 5 days" => "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 DAY)"
 );
 
 ?>
