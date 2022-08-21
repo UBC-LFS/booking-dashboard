8dfbadd39a3a7d833d87e09d6618efee83b18b48
Edmund Seow <edmund.seow@ubc.ca>
2022-08-20 21:11:52 -0700

    Change queries into array

--- include/queries.php
+++ include/queries.php
@@ -1,9 +1,8 @@
 <?php
 
-//Query all bookings for today
-$today = "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)";
-  
-//Query all bookings for next 5 days
-$next_5_days = "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 DAY)";
-  
+$query = array (
+  "today" => "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)",
+  "next_5_days" => "SELECT FROM_UNIXTIME(start_time) mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 DAY)"
+);
+
 ?>
