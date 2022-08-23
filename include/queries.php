42e66116bedf00abb8682a7d416514b2524a5ffd
Edmund Seow <edmund.seow@ubc.ca>
2022-08-23 10:12:07 -0700

    Updated query for 7 days

--- include/queries.php
+++ include/queries.php
@@ -2,7 +2,8 @@
 
 $query = array (
   "Today" => "SELECT FROM_UNIXTIME(start_time) as date_time, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)",
-  "Next 5 days" => "SELECT FROM_UNIXTIME(start_time) as date_time, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 6 DAY)"
+  "Next 7 days" => "SELECT FROM_UNIXTIME(start_time) as date_time, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 8 DAY)"
 );
 
 ?>
+
