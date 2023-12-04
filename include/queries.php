d839ca09ed1291317baf467af1a3512d13e727d0
Edmund Seow <edmund.seow@ubc.ca>
2023-12-04 14:39:11 -0800

    Moved pending query to a different var

--- include/queries.php
+++ include/queries.php
@@ -4,9 +4,11 @@ $query = array (
   "Today" => "SELECT FROM_UNIXTIME(start_time) as date_time, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE()) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 day)",
   "Next 7 days" => "SELECT FROM_UNIXTIME(start_time) as date_time, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time >= UNIX_TIMESTAMP(CURDATE() + INTERVAL 1 DAY) and start_time < UNIX_TIMESTAMP(CURDATE() + INTERVAL 8 DAY)",
   "Checked Out" => "SELECT FROM_UNIXTIME(start_time) as date_time, FROM_UNIXTIME(end_time) as check_in, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time <= UNIX_TIMESTAMP(CURDATE()) AND end_time >= UNIX_TIMESTAMP(CURDATE());"
-  "Pending" => "SELECT COUNT(*) FROM mrbs_entry WHERE status='2';"
 );
 
+$pending_q = array (
+  "Pending" => "SELECT COUNT(*) FROM mrbs_entry WHERE status='2';"
+);
 
 ?>
 
