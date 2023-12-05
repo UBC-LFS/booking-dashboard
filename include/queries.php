a59d7c9f2f417b0ec65f9cd0b5972bebead62a6a
Edmund Seow <edmund.seow@ubc.ca>
2023-12-04 16:24:13 -0800

    Changed pending query array to var

--- include/queries.php
+++ include/queries.php
@@ -6,9 +6,8 @@ $query = array (
   "Checked Out" => "SELECT FROM_UNIXTIME(start_time) as date_time, FROM_UNIXTIME(end_time) as check_in, mrbs_room.room_name, name, user_email FROM mrbs_entry JOIN mrbs_room ON mrbs_room.id = mrbs_entry.room_id WHERE start_time <= UNIX_TIMESTAMP(CURDATE()) AND end_time >= UNIX_TIMESTAMP(CURDATE());"
 );
 
-$pending_q = array (
-  "Pending" => "SELECT COUNT(*) AS cnt FROM mrbs_entry WHERE status='2';"
-);
+$pending_q = "SELECT COUNT(*) AS cnt FROM mrbs_entry WHERE status='2';"
+
 
 ?>
 
