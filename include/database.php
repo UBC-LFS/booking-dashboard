d1cb82604bd0a3c1b6bcb07f1e900e5ce558d152
Edmund Seow <edmund.seow@ubc.ca>
2023-12-04 16:04:23 -0800

    Updated database connection statements

--- include/database.php
+++ include/database.php
@@ -6,22 +6,18 @@ function get_bookings($query) {
   require "../web/sites/lc/config.inc.php";
 
   // Create connection
+  $conn = mysqli_connect($db_host, $db_login, $db_password, $db_database);
+
+  // Check connection
   $conn = mysqli_init();
 
   if (!$conn) {
-    die("mysqli_init failed");
+    die("Connection failed: " . mysqli_connect_error());
   }
 
-  // Set SSL options
-  mysqli_ssl_set($conn, $db_options['mysql']['ssl_key'], $db_options['mysql']['ssl_cert'], $db_options['mysql']['ssl_ca'], $db_options['mysql']['ssl_capath'], NULL);
+  $result =  $conn->query($query);
 
-  if (!mysqli_real_connect($conn, $db_host, $db_login, $db_password, $db_database)) {
-    die("Connect Error:  " . mysqli_connect_error());
-  }
-  
-  $result =  mysqli_query($conn, $query);
-
-  mysqli_close($conn);
+  $conn->close();
 
   return $result;
 
